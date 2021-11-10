e58ad15b7052bea840e2d5bcb60433ee797c07f3
Lyreon <78847209+lyrenox@users.noreply.github.com>
2021-11-10 12:40:51 +0800

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,26 @@
+import os
+import asyncio
+
+import nextcord
+from nextcord.ext import commands
+from dotenv import load_dotenv
+
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = nextcord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix=">", case_insensitive=True, intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} is up, nyoom!")
+
+
+bot.load_extension("cogs.utility")
+bot.load_extension("cogs.fun")
+bot.load_extension("cogs.images")
+
+bot.run(TOKEN)
